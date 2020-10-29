a75c199db00026cfb75bbdf1b61515012b15e555
jonlopez15 <63377928+jonlopez15@users.noreply.github.com>
2020-10-29 08:01:39 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -1,32 +1,36 @@
+"""Class a preexistence to a client"""
 class Client():    
-    """Class a preexistence to a client"""
+    
 
     def __init__(self, name, last_name, doc_id):
         self.name = name
         self.last_name = last_name
         self.doc_id = doc_id
         self.preexistence = []
-        
-    def add_preexistence(self, n_Preexistence):
+  
+    def add_preexistence(self, n_preexistence):
         """add a preexistence to a client"""
-        self.preexistence.append(n_Preexistence)
+        self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
     
-    def get_preexistence(self, p_Index):
+    def get_preexistence(self, p_index):
         """get a preexistence given the index"""
-        if p_Index >= len(self.preexistence):
+        if p_index >= len(self.preexistence):
             return 'There is no such preexistence'
         else:
-            return self.preexistence[pIndex]
+            return self.preexistence[p_index]
 
     def get_all_preexistence(self):
+        """get all preexistence given the index"""
         return self.preexistence
 
     def remove_preexistence(self, n_preexistence):
+        """remove a preexistence given the index"""
         self.preexistence.pop(n_preexistence)
         return len(self.preexistence) - 1
 
     def get_formatted_name(self):
+        """formatted a preexistence given the index"""
         return self.name + ' ' + self.last_name
 
 
